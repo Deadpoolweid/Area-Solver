3696a724d17068a3903327c2c9853e7d31241085
Alex_Green <deadpoolweid@yandex.ru>
2021-03-10 14:19:58 +0300

    Добавьте файлы проекта.

---
+++ Area Solver Executable/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Area_Solver_Executable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Area Solver Library/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Area_Solver_Library
+{
+    public class Class1
+    {
+    }
+}
